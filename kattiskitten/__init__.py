415eac790ebf1e044627a2a5a7dcaa49983c8931
Felix Qvist <felix.qvist@gmail.com>
2019-10-17 22:33:58 +0200

    moved submitter aswell

--- kattiskitten/__init__.py
+++ kattiskitten/__init__.py
@@ -9,16 +9,13 @@ import subprocess
 import colorful as cf
 import re
 import os
-import configparser
 import sys
 import webbrowser
-from pathlib import Path
 from kattiskitten.tester import test_problem
 from kattiskitten.scraper import get_problem_score
+from kattiskitten.submitter import submit_problem
 
 __author__ = "Felix Qvist"
-
-_HEADERS = {'User-Agent': 'kattis-cli-submit'}
 
 @click.group()
 def main():
@@ -27,55 +24,6 @@ def main():
     """
     pass
 
-def get_url(cfg, option, default):
-    if cfg.has_option('kattis', option):
-        return cfg.get('kattis', option)
-    else:
-        return 'https://%s/%s' % (cfg.get('kattis', 'hostname'), default)
-
-def get_config():
-    cfg = configparser.ConfigParser()
-
-    if not cfg.read([os.path.join(Path.home(), '.kattisrc'),
-                     os.path.join(os.path.dirname(sys.argv[0]), '.kattisrc')]):
-        raise EnvironmentError(
-            "Couldn't find .kattisrc. Please download from https://open.kattis.com/download/kattisrc")
-    return cfg
-def login(login_url, username, password=None, token=None):
-    login_args = {'user': username, 'script': 'true'}
-    if password:
-        login_args['password'] = password
-    if token:
-        login_args['token'] = token
-
-    return requests.post(login_url, data=login_args, headers=_HEADERS)
-
-def login_from_config(cfg):
-    username = cfg.get('user', 'username')
-    password = token = None
-    try:
-        password = cfg.get('user', 'password')
-    except configparser.NoOptionError:
-        pass
-    try:
-        token = cfg.get('user', 'token')
-    except configparser.NoOptionError:
-        pass
-    if password is None and token is None:
-        raise EnvironmentError('Invalid .kattisrc file')
-
-    loginurl = get_url(cfg, 'loginurl', 'login')
-    return login(loginurl, username, password, token)
-
-def open_submission(submit_response, cfg):
-    submissions_url = get_url(cfg, 'submissionsurl', 'submissions')
-
-    m = re.search(r'Submission ID: (\d+)', submit_response)
-    if m:
-        submission_id = m.group(1)
-        url = '%s/%s' % (submissions_url, submission_id)
-        webbrowser.open(url)
-            
 @main.command()
 @click.argument('problem')
 def test(problem):
@@ -141,31 +89,13 @@ for i in range(n):
 def submit(problem):
     """This command submits a problem to kattis"""
     f = f'./{problem}/main.py'
-    cfg = get_config()
-
-    login_reply = login_from_config(cfg)
-
-
-
-    data = {'submit': 'true',
-            'submit_ctr': 2,
-            'language': 'Python 3',
-            'mainclass': 'main.py',
-            'problem': problem,
-            'tag': '',
-            'script': 'true'}
 
     sub_files = []
     with open(f) as sub_file:
         sub_files.append(('sub_file[]', (os.path.basename(
             f), sub_file.read(), 'application/octet-stream')))
 
-    result = requests.post(get_url(cfg, 'submissionurl', 'submit'), data=data, files=sub_files, cookies=login_reply.cookies, headers=_HEADERS)
-
-    plain_result = result.content.decode('utf-8').replace('<br />', '\n')
-    print(plain_result)
-    open_submission(plain_result, cfg)
-
+    submit_problem(problem, sub_files)
 
 if __name__ == "__main__":
     main()
---
+++ kattiskitten/submitter.py
@@ -0,0 +1,83 @@
+import requests
+import configparser
+import os
+from pathlib import Path
+import re
+import webbrowser
+import sys
+
+_HEADERS = {'User-Agent': 'kattis-cli-submit'}
+
+def get_url(cfg, option, default):
+    if cfg.has_option('kattis', option):
+        return cfg.get('kattis', option)
+    else:
+        return 'https://%s/%s' % (cfg.get('kattis', 'hostname'), default)
+
+def get_config():
+    cfg = configparser.ConfigParser()
+
+    if not cfg.read([os.path.join(Path.home(), '.kattisrc'),
+                     os.path.join(os.path.dirname(sys.argv[0]), '.kattisrc')]):
+        raise EnvironmentError(
+            "Couldn't find .kattisrc. Please download from https://open.kattis.com/download/kattisrc")
+    return cfg
+def login(login_url, username, password=None, token=None):
+    login_args = {'user': username, 'script': 'true'}
+    if password:
+        login_args['password'] = password
+    if token:
+        login_args['token'] = token
+
+    return requests.post(login_url, data=login_args, headers=_HEADERS)
+
+def login_from_config(cfg):
+    username = cfg.get('user', 'username')
+    password = token = None
+    try:
+        password = cfg.get('user', 'password')
+    except configparser.NoOptionError:
+        pass
+    try:
+        token = cfg.get('user', 'token')
+    except configparser.NoOptionError:
+        pass
+    if password is None and token is None:
+        raise EnvironmentError('Invalid .kattisrc file')
+
+    loginurl = get_url(cfg, 'loginurl', 'login')
+    return login(loginurl, username, password, token)
+
+def open_submission(submit_response, cfg):
+    submissions_url = get_url(cfg, 'submissionsurl', 'submissions')
+
+    m = re.search(r'Submission ID: (\d+)', submit_response)
+    if m:
+        submission_id = m.group(1)
+        url = '%s/%s' % (submissions_url, submission_id)
+        webbrowser.open(url)
+            
+
+def submit_problem(problem, sub_files, open=True):
+    
+    cfg = get_config()
+
+    login_reply = login_from_config(cfg)
+
+
+
+    data = {'submit': 'true',
+            'submit_ctr': 2,
+            'language': 'Python 3',
+            'mainclass': 'main.py',
+            'problem': problem,
+            'tag': '',
+            'script': 'true'}
+
+
+    result = requests.post(get_url(cfg, 'submissionurl', 'submit'), data=data, files=sub_files, cookies=login_reply.cookies, headers=_HEADERS)
+
+    plain_result = result.content.decode('utf-8').replace('<br />', '\n')
+    print(plain_result)
+    
+    if open: open_submission(plain_result, cfg)
