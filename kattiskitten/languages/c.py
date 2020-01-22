e21ae8c87732190c143294dc82c7b8f811e2d4b8
Felix Qvist <felix.qvist@gmail.com>
2020-01-22 02:16:03 +0100

    c support

---
+++ kattiskitten/languages/c.py
@@ -0,0 +1,28 @@
+import subprocess
+import tempfile
+import os
+
+kattis_name = "C"
+file_extension = "c"
+emoji = "🥳"
+default_content = """#include <stdio.h>
+int main() {
+   printf("Hello, World!");
+   return 0;
+}
+"""
+main_class = "solution.c"
+
+def run_program(file, input_content):
+    # Make temporary path
+    dirpath = tempfile.mkdtemp()
+    compiled_path = os.path.join(dirpath, 'program')
+    subprocess.call(['gcc', file, '-o', compiled_path])
+
+    try:
+        output = subprocess.check_output([compiled_path], input=input_content)
+        return output.decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        return e.output.decode("utf-8") # We still want the output when programs exits with an error
+
+    print(f"saved to {dirpath}")
