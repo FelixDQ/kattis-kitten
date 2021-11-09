81e1d9c7c7da9b2c58fdf833299de6c484a4e324
Felix Qvist <felix.qvist@gmail.com>
2021-11-09 17:26:43 +0100

    [Rust] Added scanner util

--- kattiskitten/__init__.py
+++ kattiskitten/__init__.py
@@ -1,23 +1,26 @@
 __version__ = "0.1.0"
 
-import click
-import requests
-import zipfile
-import tempfile
-import glob
-import subprocess
-import colorful as cf
-import re
-import os
-import sys
-import webbrowser
 import configparser
-from kattiskitten.tester import test_problem
-from kattiskitten.submitter import submit_problem
-import kattiskitten.language_detector
+import glob
+import os
+import re
+import subprocess
+import sys
+import tempfile
+import webbrowser
+import zipfile
+
+import click
+import colorful as cf
+import requests
+
 import kattiskitten.config as cfg
+import kattiskitten.language_detector
+from kattiskitten.submitter import submit_problem
+from kattiskitten.tester import test_problem
 
 __author__ = "Felix Qvist"
+
 
 @click.group()
 def main():
@@ -27,18 +30,19 @@ def main():
     print(f"Using {cfg.get('active_org') or 'open.kattis.com'}")
     pass
 
+
 @main.command()
-@click.argument('problem')
+@click.argument("problem")
 def test(problem):
     """This tests a kattis problem using provided test problems"""
-    if problem == 'all':
+    if problem == "all":
         problems = glob.glob(f"./*/")
 
         completed_count = 0
         total_score = 0
         earned_score = 0
         for p in problems:
-            p = re.sub(r'[^\w]', '', p)
+            p = re.sub(r"[^\w]", "", p)
             res = test_problem(p, False)
             total_score += score
             if res:
@@ -58,89 +62,132 @@ def test(problem):
 
 @main.command()
 @click.option("--language", default="python3")
-@click.argument('problem')
+@click.argument("problem")
 def get(problem, language):
     """This command downloads a kattis problem and test files"""
-    hostname = cfg.get('active_org') if cfg.get('active_org') is not None else 'open.kattis.com'
+    hostname = (
+        cfg.get("active_org")
+        if cfg.get("active_org") is not None
+        else "open.kattis.com"
+    )
     res = requests.get(
-        f"https://{hostname}/problems/{problem}/file/statement/samples.zip")
+        f"https://{hostname}/problems/{problem}/file/statement/samples.zip"
+    )
 
     language_config = language_detector.get_config(language)
-    
+
     if res.status_code == 404:
-        print(f"Couldn't find problem '{problem}'. Maybe you typed it wrong? Or no test files exist")
+        print(
+            f"Couldn't find problem '{problem}'. Maybe you typed it wrong? Or no test files exist"
+        )
     else:
         print("Downloading samples")
         tmp_file = tempfile.NamedTemporaryFile(delete=False)
-        open(tmp_file.name, 'wb').write(res.content)
+        open(tmp_file.name, "wb").write(res.content)
 
-        with zipfile.ZipFile(tmp_file.name, 'r') as zip_ref:
-            zip_ref.extractall(f'./{problem}')
+        with zipfile.ZipFile(tmp_file.name, "r") as zip_ref:
+            zip_ref.extractall(f"./{problem}")
             print(f"Samples downloaded to './{problem}'")
         tmp_file.close()
         os.unlink(tmp_file.name)
 
-
         # Create main.xx with template
-        if not os.path.exists(f'./{problem}/solution.{language_config.file_extension}'):
-            open(f'./{problem}/solution.{language_config.file_extension}', 'w').write(language_config.default_content)
+        if not os.path.exists(f"./{problem}/solution.{language_config.file_extension}"):
+            open(f"./{problem}/solution.{language_config.file_extension}", "w").write(
+                language_config.default_content
+            )
+
+        if "additional_files" in dir(language_config):
+            for file in language_config.additional_files:
+                if not os.path.exists(f"./{problem}/{file}"):
+                    open(f"./{problem}/{file}", "w").write(
+                        language_config.additional_files[file]
+                    )
+
+        if "additional_files_that_shouldnt_be_submitted" in dir(language_config):
+            for file in language_config.additional_files_that_shouldnt_be_submitted:
+                if os.path.exists(f"./{problem}/{file}"):
+                    os.remove(f"./{problem}/{file}")
 
 
 @main.command()
-@click.argument('problem')
+@click.argument("problem")
 def submit(problem):
     """This command submits a problem to kattis"""
     lang = language_detector.determine_language(problem)
     lang_config = language_detector.get_config(lang)
-    f = f'./{problem}/solution.{lang_config.file_extension}'
+    f = f"./{problem}/solution.{lang_config.file_extension}"
 
     sub_files = []
     with open(f) as sub_file:
-        sub_files.append(('sub_file[]', (os.path.basename(
-            f), sub_file.read(), 'application/octet-stream')))
+        sub_files.append(
+            (
+                "sub_file[]",
+                (os.path.basename(f), sub_file.read(), "application/octet-stream"),
+            )
+        )
+
+    if "additional_files" in dir(lang_config):
+        for file_name in lang_config.additional_files:
+            f = f"./{problem}/{file_name}"
+            with open(f) as sub_file:
+                sub_files.append(
+                    (
+                        "sub_file[]",
+                        (
+                            os.path.basename(f),
+                            sub_file.read(),
+                            "application/octet-stream",
+                        ),
+                    )
+                )
 
     submit_problem(problem, sub_files)
+
 
 @main.group()
 def org():
     """Change organization settings"""
     pass
 
+
 @org.command()
-@click.argument('organization', required=False)
+@click.argument("organization", required=False)
 def current(organization):
     """Gets or sets the organization"""
     if not organization:
-        print("Current org is", cfg.get('active_org') or 'open.kattis.com')
-    elif cfg.sectionExists(organization) or organization == 'open.kattis.com':
-        cfg.set('active_org', organization)
+        print("Current org is", cfg.get("active_org") or "open.kattis.com")
+    elif cfg.sectionExists(organization) or organization == "open.kattis.com":
+        cfg.set("active_org", organization)
         print(f"Changed active org to {organization}")
     else:
         print("You haven't added this organization. Add it with\nkk org add")
 
+
 @org.command()
 def add():
     """Add a new organization"""
-    hostname = click.prompt('Organization url', type=str, default="open.kattis.com")
+    hostname = click.prompt("Organization url", type=str, default="open.kattis.com")
     res = requests.get(f"https://{hostname}")
     if res.status_code == 404:
-        print(f'Couldn\'t find org ({res.request.url}). Did you spell it correctly?')
+        print(f"Couldn't find org ({res.request.url}). Did you spell it correctly?")
         return
-    
+
     print("\n\nNow you need to download the configuration file from the following url:")
     print(f"https://{hostname}/download/kattisrc\n\n")
 
-    username = click.prompt('Kattis username (copy from .kattisrc)', type=str)
+    username = click.prompt("Kattis username (copy from .kattisrc)", type=str)
 
     res = requests.get(f"https://{hostname}/users/{username}")
     if res.status_code == 404:
-        print(f'Couldn\'t find user ({res.request.url}). Did you spell it correctly?')
+        print(f"Couldn't find user ({res.request.url}). Did you spell it correctly?")
         return
 
-    token = click.prompt('Kattis token (copy from .kattisrc)', type=str)
+    token = click.prompt("Kattis token (copy from .kattisrc)", type=str)
 
-    cfg.set('username', username, hostname)
-    cfg.set('token', token, hostname)
+    cfg.set("username", username, hostname)
+    cfg.set("token", token, hostname)
+
 
 if __name__ == "__main__":
     main()
--- kattiskitten/languages/rust.py
+++ kattiskitten/languages/rust.py
@@ -5,11 +5,95 @@ kattis_name = "Rust"
 file_extension = "rs"
 emoji = "⚙️"
 default_content = """
+use std::io;
+mod scanner;
+
+use scanner::Scanner;
+
 fn main() {
-    println!("Hello, world!");
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let x = scan.token::<i32>();
+    println!("{}", x);
 }
 """
 main_class = "solution.rs"
+additional_files = {
+    "scanner.rs": """
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    /// This function should be marked unsafe, but noone has time for that in a
+    /// programming contest. Use at your own risk!
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+use std::io;
+use std::str;
+
+/// Reads white-space separated tokens one at a time.
+pub struct Scanner<R> {
+    reader: R,
+    buffer: Vec<String>,
+}
+
+impl<R: io::BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buffer: vec![],
+        }
+    }
+
+    /// Use "turbofish" syntax token::<T>() to select data type of next token.
+    ///
+    /// # Panics
+    ///
+    /// Panics if there's an I/O error or if the token cannot be parsed as T.
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            self.reader.read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+    """
+}
+
+additional_files_that_shouldnt_be_submitted = {
+    "Cargo.toml": """[package]
+name = "kattis-problem" """
+}
 
 
 def run_program(file, input_content):
